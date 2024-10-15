1ef4ad4f50574aaea9db35d81d22f99f1b87fc39
Ethen <ebot14rocks@gmail.com>
2024-10-14 19:54:35 -0500

    added base web server files

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module isotope-website
+module github.com/Isotope-Robotics/Isotope-Scouting-System
 
 go 1.20
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"web"
+	"github.com/Isotope-Robotics/Isotope-Scouting-System/web"
 )
+
+const httpPort = 8080
 
 func main() {
 
 	web := web.NewWeb()
+	go web.ServeWebInterface(httpPort)
 }
